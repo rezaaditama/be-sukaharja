30efd6b7e04fdf6174f96b29a43707dafb3829d6
Reza Aditama <aditamareza03@gmail.com>
2025-06-21 14:20:51 +0700

    Connect backend to cloud database

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const dbPool = mysql.createPool({
-  host: '103.127.98.252',
-  user: 'root',
-  password: 'Baru2023',
-  database: 'db_sukaharja',
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
   waitForConnections: true,
   connectionLimit: 13,
   queueLimit: 1000,
